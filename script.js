4d46eec1dc04e4e3dc35f5d1048167dbf029cd53
deadflowers <deadflowers@users.noreply.github.com>
2025-02-05 00:50:57 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,7 @@
 (function(window) {
   var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
-
-  // Loop over the names array
   for (var i = 0; i < names.length; i++) {
-    // Retrieve the first letter of the current name
     var firstLetter = names[i].charAt(0).toLowerCase();
-
-    // Compare the 'firstLetter' to 'j' and call the appropriate speak method
     if (firstLetter === 'j') {
       byeSpeaker.speak(names[i]);
     } else {
